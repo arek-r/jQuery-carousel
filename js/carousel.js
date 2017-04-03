874701a27c12cd1c467a8eca705d154a634b77c6
Arkadiusz Rybka <arekrybka@gmail.com>
2017-04-03 22:51:53 +0200

    Prosty slider oparty na jQuery

---
+++ js/carousel.js
@@ -0,0 +1,16 @@
+$(function() {
+	var carouselList = $('#images ul');
+
+	setInterval(changeSlide, 3000);
+
+	function changeSlide() {
+		carouselList.animate({'marginLeft': -800} , 500, moveFirstSlide);	
+	};
+
+	function moveFirstSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		lastItem.after(firstItem);
+		carouselList.css({'marginLeft': 0});
+	}
+});
